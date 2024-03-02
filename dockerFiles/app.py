607111b5fd72d97cb010552280829c4200994daa
Sukhvinder Singh <46042060+Sukhvsin2@users.noreply.github.com>
2024-03-02 11:11:56 -0500

    Add files via upload

---
+++ dockerFiles/app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    return "Welcome"
+
+@app.route("/howareyou")
+def hello():
+    return "I am good, how are you?"
+
+if __name__ == "__main__":
+    app.run()
